78fb0aabc6c88c00d06532181121ac66d8a38fb6
ntnikhilesh <nikhilesh.tripathi@cognitiveclouds.com>
2019-05-03 17:30:45 +0530

    Google login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {
+  SocialLoginModule,
+  AuthServiceConfig,
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+} from "angular-6-social-login";
 
 
 @NgModule({
@@ -12,9 +18,33 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: getAuthServiceConfigs
+    }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// Social Login Configs 
+export function getAuthServiceConfigs() {
+  let config = new AuthServiceConfig(
+      [
+        // {
+        //   id: FacebookLoginProvider.PROVIDER_ID,
+        //   provider: new FacebookLoginProvider("Your-Facebook-app-id")
+        // },
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider("226656101631-hom60kj9aqdduns0ngpnl3i22j6btrdv.apps.googleusercontent.com")
+        }
+      ]
+  );
+  return config;
+}
--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AuthService,
+  FacebookLoginProvider,
+  GoogleLoginProvider
+} from 'angular-6-social-login';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private socialAuthService: AuthService) { }
 
   ngOnInit() {
+  }
+
+
+  public socialSignIn() {
+    let socialPlatformProvider;
+    socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+
+    this.socialAuthService.signIn(socialPlatformProvider).then(
+      (userData) => {
+        console.log(" sign in data : ", userData);
+      }
+    );
   }
 
 }
