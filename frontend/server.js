e631c27cb479e232e92ed8170aa50abb36817cb8
ntnikhilesh <nikhilesh.tripathi@cognitiveclouds.com>
2019-05-03 17:52:15 +0530

    Dashboard UI

--- frontend/server.js
+++ frontend/server.js
@@ -1,16 +1,27 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+// //Install express server
+// const express = require('express');
+// const path = require('path');
 
-const app = express();
+// const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/point5bn-test'));
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/point5bn-test'));
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/point5bn-test/index.html'));
-});
+// res.sendFile(path.join(__dirname+'/dist/point5bn-test/index.html'));
+// });
 
-// Start the app by listening on the default Heroku port
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
 app.listen(process.env.PORT || 8080);
+
+
